b3a7463017739d884a88432eccaf6ee1b2aafc07
Bryan Lin <78909016+bryanlin16899@users.noreply.github.com>
2022-01-02 15:59:30 +0800

    Bootcamp API,MongoDB setting

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express'); //web framework
+const dotenv = require('dotenv'); //load .env file
+const morgan = require('morgan'); //logger
+const colors = require('colors'); //terminal color
+const connectDB = require('./config/db'); //connect to mongodb
 
 // load env vars
 dotenv.config({path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
